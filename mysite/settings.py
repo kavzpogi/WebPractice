b6eaa26940581366f532031bda416f3e5efd657e
kavzpogi <guerrerokavz@gmail.com>
2019-09-08 08:13:07 -0200

    Django Practice, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd3o!x@i)_(_kxit8yg^d6v&!km7o6ao!pfv9@!*z0@sbm!b%s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
